b92e97f2e08a8ed693ebdd7c20d631a1a8dc7967
jlvilasoler <jlvilasoler@hotmail.com>
2023-04-02 01:06:29 -0300

    cargo el index

--- index.js
+++ index.js
@@ -1,158 +1,131 @@
 
-// Variables:
-let total = 0;
-let descuento = 0; // Descuento del 0% (por defecto)
-let cupones = {
-    "FIDELITY": 0.1, // Descuento del 10%
-    "VIP": 0.3 // Descuento del 30%
-};
-
-//BIENVENIDA, SE SOLICITA NOMBRE:
-let nombre = prompt("Bienvenido/a a TIENDAONLINE.UY , ¿Cuál es su nombre?");
-
-//MENSAJE DE BIENVENIDA CON NOMBRE:
-alert("Hola " + nombre + ", " + "Le damos la bienvenida a TIENDAONLINE.UY");  
-
-//SOLICITA NUMERO DE DOCUMENTO:
-let documento = prompt(nombre + ", " + "Indica su número de Documento:"); 
-
-//INICIO OPERACION DE COMPRA CON OPCIONES DE COMPRA DE PRODUCTOS:
-let mensaje = prompt("¿Desea comprar un producto? (s-si / n-no)"); 
-while (mensaje == "s" || mensaje == "S") {
-    let producto = prompt("1- Pomelo Paso de los Toros (bt 500ml)\n2- Coca Cola light (bt 600ml)\n3- Agua Salus con gas (bt 600ml)\n4- Coca Cola Regular (bt 600ml)\n5- Whisky Jhonnie Walker - Etiqueta Negra (1l)"); //Seleccion de producto
-
-    switch (producto) {
-        case "1":
-            alert("Pomelo Paso de los Toros (bt 500ml) .... $ 100,00 \nAGREGADO AL CARRITO");
-            incrementarTotal(100);
-            break;
-        case "2":
-            alert("Coca Cola light (bt 600ml) .... $ 120,00 \nAGREGADO AL CARRITO");
-            incrementarTotal(120);
-            break;
-        case "3":
-            alert("Agua Salus con gas (bt 600ml) .... $ 70,00 \nAGREGADO AL CARRITO");
-            incrementarTotal(70);
-            break;
-        case "4":
-            alert("Coca Cola Regular (bt 600ml) .... $ 120,00 \nAGREGADO AL CARRITO");
-            incrementarTotal(120);
-            break;
-        case "5":
-            alert("Whisky Johnnie Walker - Etiqueta negra (1l) .... $ 4000,00 \nAGREGADO AL CARRITO");
-            incrementarTotal(4000);
-            break;
-        default:
-            alert("Código de producto inexistente");
-            break;
+class Producto{
+    constructor(fechaCompra,idProveedor,seccion,id,nombreDeProducto,costoConIva,cantidad,plazoDePago){
+        this.fechaCompra = fechaCompra;
+        this.idProveedor = idProveedor;
+        this.seccion = seccion;
+        this.id = id;
+        this.nombreDeProducto = nombreDeProducto;
+        this.costoConIva = costoConIva;
+        this.cantidad = cantidad;
+        this.plazoDePago = plazoDePago;
+    }
+    costoSinIva(){
+        this.costoSinIva = this.costoConIva / 1.22; // COSTO MERCADERIA SIN IVA
+    }
+    precioVentaSinIva(){
+        this.precioVentaSinIva = this.costoConIva * 1.25; // PRECIO DE VENTA CONSUMIDOR SIN IVA , YA TIENE EL MARGEN INCLUIDO DE 25%
+}
+    precioVentaConIva(){
+    this.precioVentaConIva = this.precioVentaSinIva * 1.22;// PRECIO DE VENTA CONSUMIDOR CON IVA (FULL PRICE)
+}
+    margen(){
+        this.margen = (this.precioVentaConIva / this.costoConIva) -1;// % MARGEN GANANCIA
+    }
+    gananciaConIva(){
+        this.gananciaConIva = this.precioVentaConIva - this.costoConIva; // MARGEN DE GANANCIA EN PESOS CON IVA
+    }
+    gananciaSinIva(){
+        this.gananciaSinIva = this.precioVentaSinIva - this.costoSinIva; // MARGEN DE GANANCIA EN PESOS SIN IVA
+    }
+    IvaCompras(){
+        this.IvaCompras = this.costoConIva - this.costoSinIva; // IVA COMPRAS
+    }
+    IvaVentas(){
+        this.IvaVentas = this.precioVentaConIva - this.precioVentaSinIva; // IVA VENTAS
     }
 
-    mensaje = prompt("¿Desea comprar otro producto? (s-si / n-no");
+
 }
 
-//VERIFICAR SI EL USUARIO INGRESÓ UN CUPÓN VALIDO:
-let cupon = prompt("¿Tiene un cupón de descuento? (Ingrese el cupón o presione cancelar)");
-if (cupones.hasOwnProperty(cupon)) {
-descuento = cupones[cupon]; //Asignar el descuento correspondiente al cupón ingresado Tipo: "% DTO FIDELITY O VIP"
-alert("Cupón aplicado: " + cupon + " - Descuento del " + descuento*100 + "%")}
+const producto1 = new Producto("12-12-12",9991,"BEBIDAS",60001,"COCA COLA LIGHT 600ML",100,2,"120 DIAS");
+const producto2 = new Producto("12-12-12",9992,"BEBIDAS",60002,"POMELO PASO DE LOS TOROS BT.500ML",100,2,"120 DIAS");
+const producto3 = new Producto("12-12-12",9993,"BEBIDAS",60003,"COCA COLA BT 600ML",100,2,"120 DIAS");
+const producto4 = new Producto("12-12-12",9994,"BEBIDAS",60004,"WHISKY JOHNNIE WALKER-ETIQUETA NEGRA 1L",100,2,"120 DIAS");
+
+const productos = [producto1,producto2,producto3,producto4];
+console.log(productos)
 
 
-//CALCULA EL PRECIO SIN DESCUENTO Y CON DESCUENTO:
-let precioTotalSinDescuento = total; //Precio Total
-let precioTotalConDescuento = total * (1 - descuento); //Precio Total con Descuento
-let beneficio = precioTotalSinDescuento - precioTotalConDescuento; //Valor del descuento en $
 
-//NOS MUESTRA UN MENSAJE DE PRECIO TOTAL SIN DESCUENTO Y CON DESCUENTO, SI HAY:
-if (descuento > 0) {
-    alert("TOTAL DE LA COMPRA:\n\nTotal sin descuento: $" + precioTotalSinDescuento + "\nDescuento: -$" + (precioTotalSinDescuento - precioTotalConDescuento) + ("\n\nTOTAL COMPRA CON DESCUENTO: $" + precioTotalConDescuento));
-    } else {
-        alert("TOTAL DE LA COMPRA:\n\nTotal: $" + precioTotalSinDescuento);
+
+
+// 1 - FUNCIONALIDAD PARA AGREGAR PRODUCTOS COMPRADOS (INGRESO DE MERCADERIA):
+function agregarProductos(){
+    let fechaCompra = prompt("Ingrese la fecha de compra: (formato dd-mm-yy)");
+    let idProveedor = prompt("Ingrese el número de proveedor:");
+    let seccion = prompt("Ingrese sección:");
+    let id = parseInt(prompt("Ingrese el id del producto:"));
+    let nombreDeProducto = prompt("Ingrese el nombre del producto:");
+    let costoConIva = parseFloat(prompt("Ingrese el precio con iva que compramos la mercaderia:"));
+    let cantidad = parseInt(prompt("Ingrese la cantidad comprada:"));
+    let plazoDePago = prompt("Ingrese plazo de pago:");
+    productos.push(new Producto(fechaCompra,idProveedor,seccion,id,nombreDeProducto,costoConIva,cantidad,plazoDePago));
+}
+agregarProductos();
+console.log(productos);
+
+
+
+// 2 - ORDENAR EL LISTADO DE PRODUCTOS QUE TENGO EN LA LISTA
+for(const prod of productos){
+    prod.costoSinIva();
+    prod.precioVentaSinIva();
+    prod.precioVentaConIva();
+    prod.margen();
+    prod.gananciaConIva();
+    prod.gananciaSinIva();  
+    prod.IvaVentas();
+    prod.IvaCompras();
+    console.table(prod.fechaCompra + " " + prod.seccion + " " + prod.idProveedor + " " + prod.plazoDePago + " " + prod.id + " " + prod.nombreDeProducto + " " + prod.costoSinIva + " " + prod.costoConIva+ " " + prod.precioVentaSinIva + " " + prod.precioVentaConIva + " " + prod.cantidad + " " + prod.margen + " " + prod.gananciaConIva + " " + prod.gananciaSinIva + " " + prod.IvaCompras + " " + prod.IvaVentas);
+}
+
+// 3 - REDUCE:
+//REDUCE , nos dice el total de articulos que se compraron:
+const cantidadTotalArticulos = productos.reduce((sumador, Producto) => sumador + Producto.cantidad,0);
+console.log("A) LA SUMA DE TODOS LOS PRODUCTOS QUE SE COMPRARON: "+cantidadTotalArticulos);
+
+//REDUCE , nos dice el gasto total en productos acumulado:
+const TotalGastado = productos.reduce((sumador, Producto) => sumador + Producto.costoConIva,0);
+console.log("B) GASTO TOTAL PRODUCTOS COMPRADOS (en pesos): "+TotalGastado);
+
+//REDUCE , Total Iva Compras:
+const TotalIvaCompras = productos.reduce((sumador, Producto) => sumador + Producto.IvaCompras,0);
+console.log("C) TOTAL IVA COMPRAS (en pesos): "+TotalIvaCompras);
+
+//REDUCE , Total Iva Ventas:
+const TotalIvaVentas = productos.reduce((sumador, Producto) => sumador + Producto.IvaVentas,0);
+console.log("D) TOTAL IVA VENTAS (en pesos): "+TotalIvaVentas);
+
+
+
+// 4 - FIND:
+//FIND , buscaremos un numero de ID DE ARTICULO determinado, se queda con el primero id que encuentra:
+let idArticuloEncontrado = productos.find((producto) => producto.id == "60004");  
+if(idArticuloEncontrado != undefined) {
+    console.table(idArticuloEncontrado);
+}else{console.table("id articulo no encontrado");
+}
+
+
+// 5 - MAP:
+//MAP, nos da el listado de articulos con sus precios de venta :
+const listaNombreArticuloYPrecio = productos.map((producto) => {
+    return {
+        nombreProducto: producto.nombreDeProducto.toUpperCase(),
+        precioVenta: producto.precioVentaConIva
     }
-
-// MEDIO DE PAGO (TARJETAS):
-//Ingresa medio de pago:
-ingresarMedioDePago();
+});
+console.log(listaNombreArticuloYPrecio);
 
 
-//Ingresa numero tarjeta:
-function validarTarj(){
-    let tarjeta;
-
-for(i=3; i>0; i--){
-    tarjeta = prompt("Ingrese su número de tarjeta, de 16 digitos:");
-    if(tarjeta.length === 16){
-        alert("Número valido");
-        break;
-    }else if(i === 1){
-            alert("Demasiados intentos");
-        }else{
-            alert("Intente nuevamente");     
-        }
-    }
-}
-validarTarj();
-
-//Ingresa fecha vencimiento tarjeta:
-function validarVencimiento(){
-    let fechaVencimiento = prompt("Ingresar fecha de vencimiento de tarjeta:\n(indicado al dorso)");
-}
-validarVencimiento();
-
-//Ingresa codigo seguridad tarjeta de 3 digitos (al dorso):
-function codigoSeguridad(){
-    let codigo;
-for(i=3; i>0; i--){
-    codigo = prompt("Ingresar código de 3 digitos:\n(indicado al dorso de la tarjeta)");
-    if(codigo.length === 3){
-        alert("Código valido");
-        break;
-    }else if(i === 1){
-            alert("Demasiados intentos");
-        }else{
-            alert("Intente nuevamente");     
-        }
-    }
-}
-codigoSeguridad();
-
-//COSTO ENVÍO (Si corresponde), > $4000 de compra costo envío gratis:
-//Además nos muestra un alert con todos los descuentos, si corresponde:
-const envio = () => {
-    if (total >= 4000){
-        alert("Total Compra: $" + precioTotalSinDescuento + "\nDescuento: -$" + (beneficio) +"\nCosto de envío: $0\n\nTOTAL DE LA COMPRA: $" +(total-beneficio));
-    }else{
-        total += 200
-        alert("Total Compra: $" + precioTotalSinDescuento + "\nDescuento: -$" + (beneficio) + "\nCosto de envío: $200\n\nTOTAL DE LA COMPRA: $" +(total-beneficio));  
-    }
-}
-
-envio();
-
-//Solicita dirección de envio:
-let direccion = prompt(nombre + "," + " " +"nos podría indicar su dirección de envío:"); 
-
-alert("Muchas gracias por su compra "+nombre+"\n\nLo esperamos pronto...");
+// 6 - FILTER:
+//FILTER, nos devuelve la busqueda de todos los articulos que son botellas "BT":
+const buscadorNombresProductos = productos.filter((producto) => producto.nombreDeProducto.includes("BT"));
+console.log(buscadorNombresProductos);
 
 
-
-
-
-
-
-
-// FUNCIONES:
-
-// Funcion mensaje "Llevás gastado":
-function incrementarTotal(precio) {
-    total = total + precio;
-    alert("LLEVÁS GASTADO: $" + total);
-}
-
-function ingresarMedioDePago(){
-    let codigoMedioDePago = prompt("Elije el medio de pago que deseas utilizar:\n1-TARJETA DE DEBITO\n2-TARJETA DE CREDITO");
-    return codigoMedioDePago;
-}
-
-
-//////////////////
-
+// 7 - SOME:
+//SOME, nos indica por nombre , si el articulo existe o no en la base de datos (nos da true o false): 
+const existeArticulo = productos.some((producto) => producto.nombreDeProducto == "WHISKY JOHNNIE WALKER-ETIQUETA NEGRA 1L");
+console.log(existeArticulo); // RESULTADO = TRUE
