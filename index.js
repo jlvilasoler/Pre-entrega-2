c63aba11eb833a5a538d5b74388b12c1eac8f783
jlvilasoler <jlvilasoler@hotmail.com>
2023-04-02 16:12:58 -0300

    se hicieron ajustes en los prompts

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//STOCK DE PRODUCTOS POR E-COMMERCE (COMPRA Y VENTA):
+//STOCK DE PRODUCTOS POR E-COMMERCE (COMPRA Y VENTA) - PRE-ENTREGA-2 (ALUMNO: JOSE VILA):
 
 
-class Producto{
-    constructor(fechaCompra,idProveedor,numeroFactura,seccion,id,nombreDeProducto,costoConIva,cantidad,plazoDePago){
+class Producto {
+    constructor(fechaCompra, idProveedor, numeroFactura, seccion, id, nombreDeProducto, costoConIva, cantidad, plazoDePago) {
         this.fechaCompra = fechaCompra;
         this.idProveedor = idProveedor;
         this.numeroFactura = numeroFactura;
@@ -13,40 +13,40 @@ class Producto{
         this.cantidad = cantidad;
         this.plazoDePago = plazoDePago;
     }
-    costoSinIva(){
+    costoSinIva() {
         this.costoSinIva = this.costoConIva / 1.22; // COSTO MERCADERIA SIN IVA
     }
-    precioVentaSinIva(){
+    precioVentaSinIva() {
         this.precioVentaSinIva = this.costoConIva * 1.25; // PRECIO DE VENTA CONSUMIDOR SIN IVA , YA TIENE EL MARGEN INCLUIDO DE 25%
-}
-    precioVentaConIva(){
-    this.precioVentaConIva = this.precioVentaSinIva * 1.22;// PRECIO DE VENTA CONSUMIDOR CON IVA (FULL PRICE)
-}
-    margen(){
-        this.margen = (this.precioVentaConIva / this.costoConIva) -1;// % MARGEN GANANCIA
     }
-    gananciaConIva(){
+    precioVentaConIva() {
+        this.precioVentaConIva = this.precioVentaSinIva * 1.22;// PRECIO DE VENTA CONSUMIDOR CON IVA (FULL PRICE)
+    }
+    margen() {
+        this.margen = (this.precioVentaConIva / this.costoConIva) - 1;// % MARGEN GANANCIA
+    }
+    gananciaConIva() {
         this.gananciaConIva = this.precioVentaConIva - this.costoConIva; // MARGEN DE GANANCIA EN PESOS CON IVA
     }
-    gananciaSinIva(){
+    gananciaSinIva() {
         this.gananciaSinIva = this.precioVentaSinIva - this.costoSinIva; // MARGEN DE GANANCIA EN PESOS SIN IVA
     }
-    IvaCompras(){
+    IvaCompras() {
         this.IvaCompras = this.costoConIva - this.costoSinIva; // IVA COMPRAS
     }
-    IvaVentas(){
+    IvaVentas() {
         this.IvaVentas = this.precioVentaConIva - this.precioVentaSinIva; // IVA VENTAS
     }
 
 
 }
 
-const producto1 = new Producto("12-12-12",9991,1001,"BEBIDAS",60001,"COCA COLA LIGHT 600ML",100,2,"120 DIAS");
-const producto2 = new Producto("12-12-12",9992,1023,"BEBIDAS",60002,"POMELO PASO DE LOS TOROS BT.500ML",100,2,"120 DIAS");
-const producto3 = new Producto("12-12-12",9993,1011,"BEBIDAS",60003,"COCA COLA BT 600ML",100,2,"120 DIAS");
-const producto4 = new Producto("12-12-12",9994,1090,"BEBIDAS",60004,"WHISKY JOHNNIE WALKER-ETIQUETA NEGRA 1L",100,2,"120 DIAS");
+const producto1 = new Producto("12-12-12", 9991, 1001, "BEBIDAS", 60001, "COCA COLA LIGHT 600ML", 100, 2, "5");
+const producto2 = new Producto("12-12-12", 9992, 1023, "BEBIDAS", 60002, "POMELO PASO DE LOS TOROS BT.500ML", 100, 2,4);
+const producto3 = new Producto("12-12-12", 9993, 1011, "BEBIDAS", 60003, "COCA COLA BT 600ML", 100, 2,3);
+const producto4 = new Producto("12-12-12", 9994, 1090, "BEBIDAS", 60004, "WHISKY JOHNNIE WALKER-ETIQUETA NEGRA 1L", 100, 2,2);
 
-const productos = [producto1,producto2,producto3,producto4];
+const productos = [producto1, producto2, producto3, producto4];
 console.log(productos)
 
 
@@ -54,61 +54,71 @@ console.log(productos)
 
 
 // 1 - FUNCIONALIDAD PARA AGREGAR PRODUCTOS COMPRADOS (INGRESO DE MERCADERIA):
-function agregarProductos(){
+function agregarProductos() {
     let fechaCompra = prompt("Ingrese la fecha de compra: (formato dd-mm-yy)");
     let idProveedor = prompt("Ingrese el número de proveedor (Ej. 1190):");
     let numeroFactura = prompt("Ingrese el número de factura (Ej. 5052):");
-    let seccion = prompt("Ingrese sección: (Ej.BEBIDAS)");
+    let seccion = ingresarSeccion();
     let id = parseInt(prompt("Ingrese el id del producto: (EJ. 60010)"));
     let nombreDeProducto = prompt("Ingrese el nombre del producto: (Ej. SPRITE ZERO BT 1L)");
     let costoConIva = parseFloat(prompt("Ingrese el precio con iva que compramos la mercaderia: (Ej. 120)"));
     let cantidad = parseInt(prompt("Ingrese la cantidad comprada: (Ej. 10)"));
-    let plazoDePago = prompt("Ingrese plazo de pago: (Ej. 120 DIAS)");
-    productos.push(new Producto(fechaCompra,idProveedor,numeroFactura,seccion,id,nombreDeProducto,costoConIva,cantidad,plazoDePago));
+    let plazoDePago = plazoPago();
+    productos.push(new Producto(fechaCompra, idProveedor, numeroFactura, seccion, id, nombreDeProducto, costoConIva, cantidad, plazoDePago));
 }
 agregarProductos();
 console.log(productos);
 
 
+function ingresarSeccion() {
+    let seccion = prompt("Elije la seccion:\n1 - BEBIDAS\n2 - LIMPIEZA\n3 - ALMACÉN\n4 - PERFUMERÍA");
+    return seccion
+}
+
+function plazoPago() {
+    let plazoPago = prompt("Elije el plazo de pago:\n1 - CREDITO A 30 DIAS\n2 - CREDITO A 60 DIAS\n3 - CREDITO A 90 DIAS\n4 - CREDITO A 120 DIAS\n5 - CONTADO");
+    return plazoPago
+}
 
 // 2 - ORDENAR EL LISTADO DE PRODUCTOS QUE TENGO EN LA LISTA
-for(const prod of productos){
+for (const prod of productos) {
     prod.costoSinIva();
     prod.precioVentaSinIva();
     prod.precioVentaConIva();
     prod.margen();
     prod.gananciaConIva();
-    prod.gananciaSinIva();  
+    prod.gananciaSinIva();
     prod.IvaVentas();
     prod.IvaCompras();
-    console.table(prod.fechaCompra + " " + prod.seccion + " " + prod.idProveedor + " " + prod.numeroFactura + " " + prod.plazoDePago + " " + prod.id + " " + prod.nombreDeProducto + " " + prod.costoSinIva + " " + prod.costoConIva+ " " + prod.precioVentaSinIva + " " + prod.precioVentaConIva + " " + prod.cantidad + " " + prod.margen + " " + prod.gananciaConIva + " " + prod.gananciaSinIva + " " + prod.IvaCompras + " " + prod.IvaVentas);
+    console.table(prod.fechaCompra + " " + prod.seccion + " " + prod.idProveedor + " " + prod.numeroFactura + " " + prod.plazoDePago + " " + prod.id + " " + prod.nombreDeProducto + " " + prod.costoSinIva + " " + prod.costoConIva + " " + prod.precioVentaSinIva + " " + prod.precioVentaConIva + " " + prod.cantidad + " " + prod.margen + " " + prod.gananciaConIva + " " + prod.gananciaSinIva + " " + prod.IvaCompras + " " + prod.IvaVentas);
 }
 
 // 3 - REDUCE:
 //REDUCE , nos dice el total de articulos que se compraron:
-const cantidadTotalArticulos = productos.reduce((sumador, Producto) => sumador + Producto.cantidad,0);
-console.log("A) LA SUMA DE TODOS LOS PRODUCTOS QUE SE COMPRARON: "+cantidadTotalArticulos);
+const cantidadTotalArticulos = productos.reduce((sumador, Producto) => sumador + Producto.cantidad, 0);
+console.log("A) LA SUMA DE TODOS LOS PRODUCTOS QUE SE COMPRARON: " + cantidadTotalArticulos);
 
 //REDUCE , nos dice el gasto total en productos acumulado:
-const TotalGastado = productos.reduce((sumador, Producto) => sumador + Producto.costoConIva,0);
-console.log("B) GASTO TOTAL PRODUCTOS COMPRADOS (en pesos): "+TotalGastado);
+const TotalGastado = productos.reduce((sumador, Producto) => sumador + Producto.costoConIva, 0);
+console.log("B) GASTO TOTAL PRODUCTOS COMPRADOS (en pesos): " + TotalGastado);
 
 //REDUCE , Total Iva Compras:
-const TotalIvaCompras = productos.reduce((sumador, Producto) => sumador + Producto.IvaCompras,0);
-console.log("C) TOTAL IVA COMPRAS (en pesos): "+TotalIvaCompras);
+const TotalIvaCompras = productos.reduce((sumador, Producto) => sumador + Producto.IvaCompras, 0);
+console.log("C) TOTAL IVA COMPRAS (en pesos): " + TotalIvaCompras);
 
 //REDUCE , Total Iva Ventas:
-const TotalIvaVentas = productos.reduce((sumador, Producto) => sumador + Producto.IvaVentas,0);
-console.log("D) TOTAL IVA VENTAS (en pesos): "+TotalIvaVentas);
+const TotalIvaVentas = productos.reduce((sumador, Producto) => sumador + Producto.IvaVentas, 0);
+console.log("D) TOTAL IVA VENTAS (en pesos): " + TotalIvaVentas);
 
 
 
 // 4 - FIND:
 //FIND , buscaremos un numero de ID DE ARTICULO determinado, se queda con el primero id que encuentra:
-let idArticuloEncontrado = productos.find((producto) => producto.id == "60004");  
-if(idArticuloEncontrado != undefined) {
+let idArticuloEncontrado = productos.find((producto) => producto.id == "60004");
+if (idArticuloEncontrado != undefined) {
     console.table(idArticuloEncontrado);
-}else{console.table("id articulo no encontrado");
+} else {
+    console.table("id articulo no encontrado");
 }
 
 
